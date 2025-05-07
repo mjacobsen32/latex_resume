60826f327b7233b20f127b5edaeb1c31997708b0
Matthew Jacobsen <matthewjacobsen1999@gmail.com>
2025-05-07 09:12:44 -0600

    specify output filenames

--- scripts/resume.zsh
+++ scripts/resume.zsh
@@ -4,20 +4,21 @@ build_verbosity="-silent"
 
 # Function to show usage
 usage() {
-  echo "Usage: $0 [-o <output directory path>] [-v] [-h] [-r] [-c] [-f] [-a]"
+  echo "Usage: $0 [-o <output directory path>] [-n <output filename>] [-v] [-h] [-r] [-c] [-f] [-a]"
   echo "  -o            Specify the output directory (required)"
+  echo "  -n            Output filename"
   echo "  -v            Enable verbose mode"
   echo "  -h            Show this help message"
   echo "  -r            Build resume"
   echo "  -c            Build cover letter"
   echo "  -f            Build references"
   echo "  -a            Build all"
-  exit 1
 }
 
-while getopts ":o:vhcrfa" opt; do
+while getopts ":o:n:vhcrfa" opt; do
   case $opt in
     o) out_dir="$OPTARG" ;;
+    n) out_name="$OPTARG" ;;
     v) verbose=True; build_verbosity="-verbose" ;;
     h) usage ;;
     r) build_resume=True ;;
@@ -37,31 +38,37 @@ then
   echo "Please provide a name for the output dir."
   exit 1
 fi
-
 if [ ! -d "$out_dir" ]
 then
   mkdir -p $out_dir
     echo Created directory $out_dir
+fi
+# Check if output directory is provided
+if [ -z "$out_name" ]
+then
+  usage
+  echo "Please provide a name for the file names."
+  exit 1
 fi
 echo "Creating PDF's for $out_dir"
 
 if [ $build_resume ]
 then
   if [ $verbose ]; then echo "Building resume"; fi
-  latexmk -pdf -outdir=build "$build_verbosity" -jobname=resume resume.tex
-  mv -f ./build/resume.pdf $out_dir
+  latexmk -pdf -outdir=build "$build_verbosity" -jobname=${out_name} resume.tex
+  mv -f "./build/${out_name}.pdf" $out_dir
 fi
 
 if [ $build_cover_letter ]
 then
   if [ $verbose ]; then echo "Building cover letter"; fi
-  latexmk -pdf -outdir=build "$build_verbosity" -jobname=cover_letter cover_letter.tex
-  mv -f ./build/cover_letter.pdf $out_dir
+  latexmk -pdf -outdir=build "$build_verbosity" -jobname="${out_name}_cover_letter" cover_letter.tex
+  mv -f "./build/${out_name}_cover_letter.pdf" $out_dir
 fi
 
 if [ $build_references ]
 then
   if [ $verbose ]; then echo "Building references"; fi
-  latexmk -pdf -outdir=build "$build_verbosity" -jobname=references references.tex
-  mv -f ./build/references.pdf $out_dir
+  latexmk -pdf -outdir=build "$build_verbosity" -jobname="${out_name}_references" references.tex
+  mv -f "./build/${out_name}_references.pdf" $out_dir
 fi
